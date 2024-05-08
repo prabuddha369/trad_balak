ac3d6be8925d35bd0ea9091e89c041ec182d7228
prabuddha369 <prabuddhapiku@gmail.com>
2024-05-08 23:34:45 +0530

    Product page re_design

--- app/products/[id]/page.jsx
+++ app/products/[id]/page.jsx
@@ -3,10 +3,10 @@ import React, { useState } from 'react';
 import Header from '@/components/shared/Header'
 import Footer from '../../../components/shared/Footer'
 import { GrFormNextLink } from "react-icons/gr";
-import { Separator } from "@/components/ui/separator";
 import { GrFormPreviousLink } from "react-icons/gr";
-import { Dancing_Script } from 'next/font/google'
 import Carousel from 'react-material-ui-carousel'
+import { FaRegCircle } from "react-icons/fa";
+import { Dancing_Script } from 'next/font/google'
 import { asthetic, vintage, couples, fullsleves } from '@/constants';
 import Image from 'next/image';
 
@@ -32,7 +32,7 @@ export default function Page({ params }) {
 
     const product = findProductById(productId);
 
-    //+ and - for Qauntity
+    //+ and - for Quantity
     const [quantity, setQuantity] = useState(1);
 
     const increaseQuantity = () => {
@@ -64,13 +64,19 @@ export default function Page({ params }) {
 
     return (
         <section className='relative'>
-            {/* <div className='absolute top-[180px] w-fit h-[160px] transform -rotate-90 text-xl text-white'>READ TO SUMMERS</div> */}
+            <div className='absolute top-[160px] w-fit h-[180px] transform -rotate-90 text-2xl text-white'>Road to summers</div>
             <Header />
             {product ? (
                 <div>
-                    <div className="pt-[30%] px-[7%] md:pt-[10%] bg-white text-black">
+                    <div className={`pt-[30%] md:pt-[10%]`} style={{ background: `url(/bg.png) no-repeat center`, backgroundSize: '', }}>
+                        <div className='flex justify-end me-5'>
+                            <div className='h-fit  font-bold flex flex-col'>
+                                <p className='text-2xl pb-2 ms-20'>{product.name}</p>
+                                <p className='bg-white text-white text-[6px] ms-14 me-32'>underline</p>
+                            </div>
+                        </div>
                         <Carousel
-                            className='flex flex-col w-[100%]'
+                            className='flex flex-col w-[100%] mt-5'
                             autoPlay={true}
                             interval={5000}
                             swipe={true}
@@ -81,8 +87,22 @@ export default function Page({ params }) {
                                     background: "#ffffff00",
                                 }
                             }}
-                            NextIcon={<GrFormNextLink size={30} className='text-black' />}
-                            PrevIcon={<GrFormPreviousLink size={30} className='text-black' />}
+                            IndicatorIcon={<FaRegCircle size={14}/>}
+                            indicatorIconButtonProps={{
+                                style: {
+                                    padding: '1px',  
+                                    margin: '5px'  ,
+                                    color: 'white'     
+                                }
+                            }}
+                            activeIndicatorIconButtonProps={{
+                                style: {
+                                    backgroundColor: 'white',
+                                    color:'grey'
+                                }
+                            }}
+                            NextIcon={<GrFormNextLink size={30} className='text-[#ffffff]/0' />}
+                            PrevIcon={<GrFormPreviousLink size={30} className='text-[#ffffff]/0' />}
                         >
                             {product.imgUrl.map((imgurl, index) => (
                                 <div className='flex w-[100%] place-items-center justify-center text-center'>
@@ -92,97 +112,106 @@ export default function Page({ params }) {
                                         height="0"
                                         sizes="100vw"
                                         className="w-[40vh] h-[60vh] md:w-1/4 md:h-[40%] mt-3 rounded-xl" />
+
+                                    <div className='absolute right-0 mt-80 ps-14' style={{ background: `url(/solution_bg.png) no-repeat left`, }}>
+                                            {product.solutions.map((solution, index) => (
+                                                <p key={index}>{solution}</p>
+                                            ))}
+                                    </div>
                                 </div>
                             ))}
                         </Carousel>
-                        <div className='h-fit  font-medium flex flex-col'>
-                            <h5 className='text-xs py-5'>TRADBALAK CLOTHING</h5>
-                            <h1 className='text-3xl'>{product.name}</h1>
-                            <div className='flex flex-row font-light mt-5 gap-2 place-items-end'><p className='text-xl'>{product.price}</p> <p className='text-base font-medium text-[#8a8a8a]'>M.R.P</p> {product.sizes[selectedSize] ? <p className='bg-[#584ea6] px-2 text-white font-bold'>Sold Out</p> : <p></p>} </div>
-                            <p className='text-sm font-light'>&#40;incl. of all <b>taxes</b>&#41;</p>
+
+                        <div className='flex flex-row mx-5 mt-8 justify-between'>
+                            <h1 className='text-5xl font-bold'>{product.price}</h1>
                         </div>
-                        <Separator className="border border-gray-200 my-5" />
-                        <div className='flex flex-col'>
-                            <p className='text-lg font-medium pb-2'>Size: {selectedSize}</p>
-                            <div className='w-full flex flex-wrap text-black justify-start place-items-center gap-2'>
-                                {Object.keys(product.sizes).map((size, index) => (
-                                    <div key={index}
-                                        onClick={() => handleSizeClick(size)}
-                                        className={`w-12 h-12 md:w-10 md:h-10 rounded-[5px] text-base flex flex-wrap border-grey-800 justify-center ${selectedSize === size ? " border-[#363636] border-[2px]" : ""}  ${selectedSize === size && !product.sizes[size] ? "bg-gray-200 border-[#363636] border-[2px]" : ""} font-light place-items-center border-[1px] ${product.sizes[size] ? "text-[#D3D3D3] line-through" : ""}`}>
-                                        {size}
-                                    </div>
-                                ))}
+                        <div className='mx-5 mt-5'>
+                            <input type='button' value="PRE ORDER" onClick={() => alert("Coming soon!")} className='bg-white text-black text-2xl py-4 font-bold w-full rounded-lg' />
+                        </div>
+                        <div className='mx-5 mt-3'>
+                            <input type='button' value="ADD TO CART" onClick={() => alert("Coming soon!")} className='bg-[#ab2424] text-white text-2xl py-2 font-bold w-full rounded-lg' />
+                        </div>
+                        <div className='flex flex-row justify-between mx-5 mt-5 pb-10'>
+                            <div className='flex flex-col w-[70%]'>
+                                <p className='text-xl pb-2'>Size</p>
+                                <div className='w-full flex flex-wrap text-white justify-start place-items-center gap-2'>
+                                    {Object.keys(product.sizes).map((size, index) => (
+                                        <div key={index}
+                                            onClick={() => handleSizeClick(size)}
+                                            className={`w-7 h-8 md:w-10 md:h-10 text-sm flex flex-wrap border-white justify-center ${selectedSize === size && !product.sizes[size] ? "bg-white text-black" : ""} font-semibold place-items-center border-[2px] ${product.sizes[size] ? "text-[#D3D3D3] line-through" : ""}`}>
+                                            {size}
+                                        </div>
+                                    ))}
+                                </div>
                             </div>
-                        </div>
-                        <div className='flex flex-col my-5'>
-                            <p className='text-lg pb-2'>Quantity:</p>
-                            <div className="w-fit rounded-[5px] py-2 flex items-center bg-white text-black font-bold border border-gray-200 border-[3px]">
-                                <button onClick={decreaseQuantity} className="px-4 font-bold text-xl">
-                                    -
-                                </button>
-                                <span className="px-6 font-light">{quantity}</span>
-                                <button onClick={increaseQuantity} className="px-4 font-bold text-xl">
-                                    +
-                                </button>
+                            <div className='flex flex-col'>
+                                <p className='text-xl pb-2'>Quantity</p>
+                                <div className="flex items-center bg-white text-black font-bold rounded-xl">
+                                    <button onClick={decreaseQuantity} className="px-2 py-1 border border-gray-400 rounded-l">
+                                        -
+                                    </button>
+                                    <span className="px-4 py-1 border border-gray-400">{quantity}</span>
+                                    <button onClick={increaseQuantity} className="px-2 py-1 border border-gray-400 rounded-r">
+                                        +
+                                    </button>
+                                </div>
                             </div>
-                        </div>
-                        <div className='mx-5 mt-3 bg-[#dbdbdb] text-black rounded-[5px]'>
-                            <input
-                                type='button'
-                                value={product.sizes[selectedSize] ? "SOLD OUT" : "ADD TO CART"}
-                                onClick={() => alert("Coming soon!")}
-                                className={`text-sm py-3 w-full ${product.sizes[selectedSize] ? "font-medium" : "font-bold"}`}
-                            />
                         </div>
 
-                        <div className={`mx-5 mt-3 text-white ${product.sizes[selectedSize] ? "bg-[#636363]" : "bg-black"}`}>
-                            <input type='button' value="BUY NOW" onClick={() => alert("Comming soon!")} className='text-sm py-3 font-bold w-full' />
-                        </div>
-                        <Separator className="border border-gray-200 mt-8" />
-                    </div>
-                    <div className='bg-white flex flex-row pt-5 px-7'>
-                        <div className='flex flex-col w-[45%]'>
-                            <p className={`${dancing_script.className} text-5xl text-black font-bold text-end`}>Product</p>
-                            <p className='text-xl text-black font-bold text-end'>Desctiption</p>
-                            <div className='relative'>
-                                <Image src={product.productdesc1imgUrl} alt={product.name}
-                                    width="0"
-                                    height="0"
-                                    sizes="100vw"
-                                    className="w-full h-[18vh] md:w-1/4 md:h-[40%]" />
-                                <Image src={product.productdesc2imgUrl} alt={product.name}
-                                    width="0"
-                                    height="0"
-                                    sizes="100vw"
-                                    className="absolute top-24 left-32 w-[25vh] h-[15vh]" />
+                        <div className='bg-[#5A5A5A]/50 rounded-t-xl flex flex-col'>
+                            <div className='flex flex-row pt-5 ps-5'>
+                                <div className='bg-gradient-to-r from-[#ff5c00]/40 via-[#ff3131]/40 to-[#760000]/70 flex flex-row w-[100%] pb-32 rounded-l-2xl p-3'>
+                                    <div className='flex flex-col'>
+                                        <p className={`${dancing_script.className} text-5xl text-white font-bold text-end`}>Product</p>
+                                        <p className='text-xl text-white font-bold text-end'>Description</p>
+                                    </div>
+                                    <div className='text-white ms-16 pt-5'>
+                                        <ul className='list-disc'>
+                                            {product.desctiptions.map((description, index) => (
+                                                <li key={index}>{description}</li>
+                                            ))}
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div className='absolute left-0'>
+                                    <Image src='/fabric.png' alt={product.name}
+                                        width="0"
+                                        height="0"
+                                        sizes="100vw"
+                                        className="mt-24 w-[24vh] h-[16vh] md:w-1/4 md:h-[40%]" />
+                                    <Image src='/cotton.png' alt={product.name}
+                                        width="0"
+                                        height="0"
+                                        sizes="100vw"
+                                        className="absolute top-24 mt-14 left-32 w-[25vh] h-[15vh]" />
+                                </div>
                             </div>
-                        </div>
-                        <div className='text-black ms-10 pt-12'>
-                            <ul className='list-disc'>
-                                {product.desctiptions.map((description, index) => (
-                                    <li key={index}>{description}</li>
-                                ))}
-                            </ul>
-                        </div>
-                    </div>
-                    <div className='bg-[#D3D3D3] py-32'>
-                        <div className='relative bg-white w-[90%] flex flex-row'>
-                            <div className='text-black ps-10 pt-10 pb-10 w-[60%]'>
-                                <ul className='list-disc'>
-                                    {product.instructions.map((description, index) => (
-                                        <li key={index}>{description}</li>
-                                    ))}
-                                </ul>
+
+                            <div className='py-20 pb-32'>
+                                <div className='relative bg-gradient-to-r from-[#760000]/70 via-[#ff3131]/40 to-[#ff5c00]/40 w-[90%] rounded-r-2xl flex flex-row'>
+                                    <div className='text-white ps-10 pt-10 pb-16 w-[60%]'>
+                                        <ul className='list-disc'>
+                                            {product.instructions.map((description, index) => (
+                                                <li key={index}>{description}</li>
+                                            ))}
+                                        </ul>
+                                    </div>
+                                    <div className='pt-2 w-[40%] pe-2'>
+                                        <p className={`${dancing_script.className} text-5xl text-white font-bold text-end`}>Product</p>
+                                        <p className='text-xl text-white font-bold text-center'>Care</p>
+                                    </div>
+                                    <Image src='/stained.png' alt={product.name}
+                                        width="0"
+                                        height="0"
+                                        sizes="100vw"
+                                        className="absolute top-24 -right-10 w-[24vh] h-[18vh]" />
+                                    <Image src='/washing.png' alt={product.name}
+                                        width="0"
+                                        height="0"
+                                        sizes="100vw"
+                                        className="absolute top-24 mt-24 me-20 -right-10 w-[24vh] h-[18vh]" />
+                                </div>
                             </div>
-                            <div className='pt-2 w-[40%] pe-2'>
-                                <p className={`${dancing_script.className} text-5xl text-black font-bold text-end`}>Product</p>
-                                <p className='text-xl text-black font-bold text-center'>Care</p>
-                            </div>
-                            <Image src={product.productcareimgUrl} alt={product.name}
-                                width="0"
-                                height="0"
-                                sizes="100vw"
-                                className="absolute top-24 -right-10 w-[30vh] h-[25vh]" />
                         </div>
                     </div>
                 </div>
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -2,11 +2,10 @@
 import React, { useEffect, useState } from "react";
 import { Dancing_Script } from 'next/font/google'
 import { EB_Garamond } from 'next/font/google'
-import { VscAccount } from "react-icons/vsc";
-import { BsCartPlus } from "react-icons/bs";
+import { IoBag } from "react-icons/io5";
+import { AiOutlineMenu } from "react-icons/ai";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Separator } from "@/components/ui/separator";
-import { RiMenuUnfoldLine } from "react-icons/ri";
 import NavItems from "./NavItems";
 import Image from "next/image";
 
@@ -17,39 +16,41 @@ const Header = () => {
     const [isOpen, setOpen] = useState(false);
 
     return (
-        <header className='fixed z-10 top-0 w-full h-fit py-4 flex flex-row border-b border-[#d9d9d9] justify-between place-items-center bg-white text-black'>
-            <nav className="ms-5">
-                <Sheet open={isOpen} onOpenChange={setOpen}>
-                    <SheetTrigger className="align-middle">
-                        <RiMenuUnfoldLine size={30} />
-                    </SheetTrigger>
-                    <SheetContent
-                        className="flex flex-col bg-white w-[1/3] text-black"
-                        side={"left"}
-                    >
-                        <div className="flex gap-5 place-items-center">
-
-                            <span className="text-2xl flex flex-row place-items-center">
-                                <p className={`${eb_garamond.className} text-3xl pe-2`}>TRAD</p>
-                                <p className={`text-[#bfa163] ${dancing_script.className}`}>Balak</p>
-                            </span>
-                        </div>
-                        <Separator className="border border-gray-50/80" />
-                        <NavItems setOpen={setOpen} />
-                    </SheetContent>
-                </Sheet>
-            </nav>
-            <div>
+        <header className='fixed z-10 top-0 w-full h-fit py-4 flex flex-row border-b border-[#d9d9d9] justify-between place-items-center bg-[#ab2424] text-white'>
+            <div className="ms-5">
                 <div className='flex flex-row text-3xl md:text-5xl gap-2 place-items-center'>
                     <p className={`${eb_garamond.className} text-4xl md:text-6xl`}>TRAD</p>
-                    <p className={`text-[#bfa163] ${dancing_script.className}`}>Balak</p>
+                    <p className={`text-white ${dancing_script.className}`}>Balak</p>
                 </div>
                 <p>Elegance Redefined Here</p>
             </div>
 
-            <div className='flex flex-row me-5 gap-5'>
-                <VscAccount size={30} />
-                <BsCartPlus size={30} />
+            <div className="flex flex-row gap-5">
+                <nav>
+                    <Sheet open={isOpen} onOpenChange={setOpen}>
+                        <SheetTrigger className="align-middle">
+                            <AiOutlineMenu size={30} />
+                        </SheetTrigger>
+                        <SheetContent
+                            className="flex flex-col bg-white w-[1/3] text-black"
+                            side={"left"}
+                        >
+                            <div className="flex gap-5 place-items-center">
+
+                                <span className="text-2xl flex flex-row place-items-center">
+                                    <p className={`${eb_garamond.className} text-3xl pe-2`}>TRAD</p>
+                                    <p className={`text-[#bfa163] ${dancing_script.className}`}>Balak</p>
+                                </span>
+                            </div>
+                            <Separator className="border border-gray-50/80" />
+                            <NavItems setOpen={setOpen} />
+                        </SheetContent>
+                    </Sheet>
+                </nav>
+
+                <div className='flex flex-row me-5 gap-5'>
+                    <IoBag size={30} />
+                </div>
             </div>
         </header>
     )
